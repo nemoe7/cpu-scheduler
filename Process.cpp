ad7562b503a71960f1b5f553f0f9f175ab26e990
stanpai78 <stanpai78@gmail.com>
2024-06-20 18:00:19 +0800

    Update Process to create file in constructor instead of execution

--- Process.cpp
+++ Process.cpp
@@ -10,6 +10,15 @@
 typedef std::string String;
 
 Process::Process(int pid, String name, bool filler) : _pid(pid), _name(name) {
+    String filename = ".\\output\\print_" + std::to_string(this->_pid) + ".txt";
+    std::ofstream output;
+    output.open(filename, std::ios::out);
+    if (output.is_open()) {
+        output << "Process name: " << this->_name << std::endl
+            << "Logs:" << std::endl
+            << std::endl;
+        output.close();
+    }
     if (filler) {
         for (int i = 0; i < 100; i++) {
             this->_commandList.push_back(
@@ -22,17 +31,6 @@ Process::Process(int pid, String name, bool filler) : _pid(pid), _name(name) {
 }
 
 void Process::execute() {
-    if (this->_commandCounter == 0) {
-        String filename = ".\\output\\print_" + std::to_string(this->_pid) + ".txt";
-        std::ofstream output;
-        output.open(filename, std::ios::out);
-        if (output.is_open()) {
-            output << "Process name: " << this->_name << std::endl
-                << "Logs:" << std::endl
-                << std::endl;
-            output.close();
-        }
-    }
 
     if (!this->hasFinished()) {
         this->_commandList.at(_commandCounter)->execute(this->_cpuCoreID);
